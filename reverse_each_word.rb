113f136a94b30669702ca697132e706d06061333
Newton Ombese <newtonombese1@gmail.com>
2022-08-20 08:19:47 +0300

    coded along

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,11 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = words.map(&:reverse).join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
+# binding.pry
+# 0
